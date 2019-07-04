aa39503ba415598f1bab8da28b3a01a3bcb1b947
Edin Jakupovic <edin.jakupovic@trustly.com>
2019-07-04 14:20:11 +0200

    Created skeleton for mainn logic, fetching refresh token lazily,

--- psnclient.py
+++ psnclient.py
@@ -1,6 +1,35 @@
 
 from tokenfetcher import TokenLoader
+from psnapi.Friend import Friend
+from psnapi.Auth import Auth
+import time
+import json
 
-token_loader = TokenLoader()
-# this is broken, due to 2fa missing
-token_loader.fetch_and_write_token()
+
+def main():
+  saftblandare_triggered = False
+
+  token_loader = TokenLoader()
+  token = token_loader.get_tokens()
+
+  friends = Friend(token)
+
+  while True:
+    print("loop")
+    json_friends = friends.my_friends()
+    print(json.dumps(json_friends))
+    if is_playing_fifa(json_friends) and not saftblandare_triggered:
+      trigger_saftblandare()
+    time.sleep(15)
+
+
+
+
+
+def is_playing_fifa(data):
+  return True
+
+def trigger_saftblandare():
+  return True
+
+main()
--- tokenfetcher.py
+++ tokenfetcher.py
@@ -1,41 +1,31 @@
 from psnapi.Auth import Auth
-from psnapi.Friend import Friend
-from psnapi.User import User
-from psnapi.Messaging import Messaging
 import json
 import os
 
 
 class TokenLoader:
 
+  tokens = None
 
-  def get_credentials_as_json(self):
+  def _get_credentials_as_json(self):
     with open('credentials/info.json') as credentials:
       return json.loads(credentials.read())
 
-
-  def fetch_and_write_token(self):
-    logininfo = self.get_credentials_as_json()
+  def _fetch_and_write_token(self):
+    logininfo = self._get_credentials_as_json()
     print(json.dumps(logininfo))
-    print(logininfo['2FA'])
     auth = Auth(
         logininfo['YOUR_EMAIL'],
         logininfo['YOUR_PASSWORD'],
         logininfo['TICKET_UUID'],
         logininfo['2FA']
     )
-
     tokens = auth.get_tokens()
-
-
     f = open('tokens', 'wt', encoding='utf-8')
     f.write(json.dumps(tokens))
     f.close()
 
-  def tokens_exist(self):
-    return os.path.isfile("tokens")
-
-  def get_tokens(self):
+  def _get_tokens(self):
     with open('tokens', encoding='utf-8') as data_file:
       data = json.loads(data_file.read())
       new_token_pair = Auth.GrabNewTokens(data['refresh'])
@@ -45,5 +35,15 @@ class TokenLoader:
         "refresh": new_token_pair[1],
         "npsso": data['npsso'] # saved above!
       }
+      print(tokens)
       return tokens
+
+  def get_tokens(self):
+    if self.tokens is not None:
+      return self.tokens
+    else:
+      if not os.path.isfile("tokens"):
+        self._fetch_and_write_token()
+      self.tokens = self._get_tokens()
+      return self._get_tokens()
 
