8540ecabdfeb8e0bc3f5e2120e9e43abcb63d44f
Edin Jakupovic <edin.jakupovic@trustly.com>
2019-07-05 12:47:08 +0200

    Did stuff

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -2,69 +2,123 @@ import React from 'react';
 import './App.css';
 
 
-let click = () => {
-};
+let validCharsRegex = 'a-zA-Z0-9  .,-:';
+class GameList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mostplayed: []
+    }
+  }
 
+  pollingTimer;
 
+  componentDidMount() {
+    let mostPlayedStatusChecker = () => getMostPlayed(data => {
+      console.log(data);
+      const gameNames = Object.keys(data);
+      const duration = Object.values(data);
 
-let history = [{
-    "game":"fifa",
-    "duration": 12
-  }, {
-    "game": "smash",
-    "duration": 43
-  }];
+      let newState = [];
+      for (let i = 0; i < gameNames.length; i++) {
+        newState.push({
+          game: removeChars(validCharsRegex ,gameNames[i]),
+          duration: duration[i] + " hours"
+        })
+      }
 
-let historyItems = history.map((game) =>
-    <li key={game.game}><p>Game: {game.game}</p>  <p>Duration: {game.duration}</p></li>
-);
+      this.setState({
+        mostplayed: newState.filter(gameRow => gameRow.game !== "")
+      });
 
-function GameList(){
-    return (
-        <ul>{historyItems}</ul>
-    )
-}
+    });
+    mostPlayedStatusChecker();
+    this.pollingTimer = setInterval(() => mostPlayedStatusChecker(), 5000)
+  }
 
-let gameBeingPlayed = false;
-function GameStatus() {
-    return (
-        <p>Game Being Played {gameBeingPlayed ? "YES":"NO"}</p>
-    )
-}
+  componentWillUnmount() {
+    clearInterval(this.pollingTimer);
+  }
 
-function TestButton() {
-  return (
-      <button onClick={click}>Tetss</button>
-  )
-}
-function getMostPlayed() {
-  console.log("Fetching most played");
-  fetch('http://localhost:5000/psn/mostplayed')
-  .then(data => data.json())
-  .then(jsonData => console.log(jsonData))
-}
-
-
-class App extends React.Component{
-
-  componentDidMount(){
-    let pollingTimer = setInterval(() => getMostPlayed(), 5000)
+  prettyprint() {
+    return this.state.mostplayed.map((game) =>
+        <li key={game.game}><p>Game: {game.game}</p>
+          <p>Duration: {game.duration}</p></li>
+    );
   }
 
   render() {
     return (
-    <div className="App">
-      <header className="App-header">
-        <div>
-          <TestButton/>
-          <GameStatus/>
+        <ul>{this.prettyprint()}</ul>
+    )
+  }
+}
+
+function removeChars(validChars, inputString) {
+  var regex = new RegExp('[^' + validChars + ']', 'g');
+  return inputString.replace(regex, '');
+}
+
+let gameBeingPlayed = false;
+
+class GameStatus extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gameStatus: {}
+    }
+  }
+
+  pollingTimer;
+
+  componentDidMount() {
+    let statusCheck = () => getCurrentStatus(data => {
+      this.setState({
+        gameStatus: data
+      })
+    });
+    statusCheck();
+    this.pollingTimer = setInterval(() => statusCheck(), 5000)
+  }
+
+  render() {
+    return (<p>Game Being Played: {
+      this.state.gameStatus['TrustlyStockholm']
+          ? removeChars(validCharsRegex,this.state.gameStatus['TrustlyStockholm'])
+          : "None"
+    }</p>)
+  }
+}
+
+function getCurrentStatus(callback) {
+  console.log("Fetching current played");
+  fetch('http://localhost:5000/psn/played')
+  .then(data => data.json())
+  .then(jsonData => callback(jsonData))
+}
+
+function getMostPlayed(callback) {
+  console.log("Fetching most played");
+  fetch('http://localhost:5000/psn/mostplayed')
+  .then(data => data.json())
+  .then(jsonData => callback(jsonData))
+}
+
+class App extends React.Component {
+
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <div>
+              <GameStatus/>
+            </div>
+            <div>
+              <GameList/>
+            </div>
+          </header>
         </div>
-        <div>
-          <GameList/>
-        </div>
-      </header>
-    </div>
-  )
+    )
   }
 }
 
